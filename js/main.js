4ab865679285e6edab732fa942a6cb1475270074
Alex Smith <alex@avsmith.net>
2014-10-05 12:11:49 +0100

    Tidy Up

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ requirejs.config({
   baseUrl: 'js'
 });
 
-window.onload = require(['config', 'dom', 'Grid'],
-                function(config, dom, Grid) {
+window.onload = require(['dom', 'Grid'],
+                function(dom, Grid) {
 
-  var grid = new Grid(config.gridSize).randomise(3);
+  var grid = new Grid(100).randomise(3);
   dom.createGridContainer().displayGrid(grid);
 
 });
--- test/logic-spec.js
+++
@@ -1,7 +0,0 @@
-// define(['logic'], function(logic) {
-//   describe("Logic", function() {
-//     it("contains spec with an expectation", function() {
-//       pending();
-//     });
-//   });
-// });
